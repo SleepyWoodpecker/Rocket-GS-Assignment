04cca7e6cd132b557bf8f319d5712fad0efb9b06
euanlimzx <euanlimzx@gmail.com>
2025-02-01 14:58:03 -0800

    minor rearrangements

--- Grafana_Backend/Grafana_From_CSV.py
+++ Grafana_Backend/Grafana_From_CSV.py
@@ -1,26 +1,12 @@
 # stream data to grafana at a rate of 10hz, which is once every every 0.1 seconds / 100ms
 
 import csv, time, socket
-
-pt_keys = ["pt0", "pt1", "pt2", "pt3", "pt4", "pt5", "pt6", "pt7", "pt8", "pt9", "lc1"]
-pt_channel = "sensorvals"
-
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
 pressure_transducer_port = ("127.0.0.1", 4001)
 octocoupler_port = ("127.0.0.1", 65415)
 
-
-def format_list_to_influx(
-    data: list[int | float], keys: list[str], channel: str, current_time: int
-) -> bytes:
-    data_string = ""
-    for key, val in zip(keys, data[1:]):
-        data_string += f"{key}={val},"
-    data_string = data_string.strip(",")
-
-    return (channel + " " + data_string + " " + str(current_time)).encode()
-
+pt_keys = ["pt0", "pt1", "pt2", "pt3", "pt4", "pt5", "pt6", "pt7", "pt8", "pt9", "lc1"]
+pt_channel = "sensorvals"
 
 def main(pt_file: csv.DictReader, thrust_file: csv.DictReader):
     # skip past the header columns
@@ -43,6 +29,17 @@ def main(pt_file: csv.DictReader, thrust_file: csv.DictReader):
 
         time.sleep(0.1)
 
+def format_list_to_influx(
+    data: list[int | float], keys: list[str], channel: str, current_time: int
+) -> bytes:
+    print(data)
+    print(keys)
+    data_string = ""
+    for key, val in zip(keys, data[1:]):
+        data_string += f"{key}={val},"
+    data_string = data_string.strip(",")
+
+    return (channel + " " + data_string + " " + str(current_time)).encode()
 
 if __name__ == "__main__":
     with open("../data/pt_values.csv", "r") as pt_csv_file:
